d3200a5c176d6292077d1a6f175f7d13571de990
Yves Dorfsman <yves@zioup.com>
2019-07-01 23:28:40 -0600

    initial commit

---
+++ idempotent/runall.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+cd "$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+if [[ ! -f $(basename ${BASH_SOURCE[0]}) ]] ; then
+    echo -e "\nSomething went wrong" >&1
+    echo -e "Lost myself in directory \'$PWD\'" >&1
+    echo -e "Aborting...\n" >&1
+    exit 1
+fi
+
+for f in [0-9]*
+do
+    ./$f
+done
+
